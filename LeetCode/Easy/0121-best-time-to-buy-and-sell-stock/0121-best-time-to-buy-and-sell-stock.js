fc4724e292133a422b4d78c3f7532e5b2d94bd4c
zzinLee <91821887+zzinLee@users.noreply.github.com>
2024-03-29 17:00:35 +0900

    Time: 78 ms (39.84%), Space: 59.5 MB (22.80%) - LeetHub

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,17 +3,12 @@
  * @return {number}
  */
 function maxProfit (prices) {
-  let buy = prices[0];
+  let minPrice= prices[0];
   let maxProfit = 0;
 
   for (let day = 1; day < prices.length; day++) {
-    const today = prices[day];
-
-    if (today < buy) {
-      buy = today;
-    } else if (today - buy > maxProfit) {
-      maxProfit = today - buy;
-    }
+    minPrice = Math.min(minPrice, prices[day]); 
+    maxProfit = Math.max(maxProfit, prices[day] - minPrice);
   }
 
   return maxProfit;

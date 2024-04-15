578f52ee8fa73ce7f6b57201a1cc0f1a30e98a17
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-04-15 11:38:17 +0900

    Time: 55 ms (82.32%), Space: 51.7 MB (51.95%) - LeetHub

---
+++ LeetCode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
+  
+  if (!root.left && !root.right) {
+    return 1;
+  }
+  
+  if (root.left && root.right) {
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  }
+  
+  if (root.left) {
+    return maxDepth(root.left) + 1;
+  }
+  
+  if (root.right) {
+    return maxDepth(root.right) + 1;
+  }
+}

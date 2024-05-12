926d15f6761ea6d7a7d5341165d9ae0eae143285
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-05-12 15:48:20 +0900

    Time: 71 ms (100.00%), Space: 51.9 MB (100.00%) - LeetHub

---
+++ LeetCode/Easy/3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var findPermutationDifference = function(s, t) {
+  const sMap = new Map();
+  const tMap = new Map();
+  let ans = 0;
+  
+  for (let i = 0; i < s.length; i++) {
+    sMap.set(s[i], i);
+    tMap.set(t[i], i);
+  }
+  
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i];
+    const index = tMap.get(ch);
+    
+    ans += Math.abs(i - index);
+  }
+  
+  return ans;
+};

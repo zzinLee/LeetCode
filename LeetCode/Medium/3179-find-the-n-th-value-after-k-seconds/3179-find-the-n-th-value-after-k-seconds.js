ac671667d2d87daccc849d2c529419c4908defa7
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-06-11 10:23:09 +0900

    Time: 335 ms (69.23%), Space: 52.4 MB (45.51%) - LeetHub

--- LeetCode/Medium/3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.js
+++ LeetCode/Medium/3179-find-the-n-th-value-after-k-seconds/3179-find-the-n-th-value-after-k-seconds.js
@@ -6,16 +6,13 @@
 
 var valueAfterKSeconds = function(n, k) {
   const arr = new Array(n).fill(1);
-  let ans = arr;
   
-  for (let i = 0; i < k; i++) {
-    for (let index = 1; index < n; index++) {
-      const up = ans[index];
-      const left = ans[index - 1];
-      
-      ans[index] = (up + left) % (1E9 + 7);
+  for (let i = 0; i < k - 1; i++) {   
+    for (let j = 1; j < arr.length; j++) {
+      arr[j] += arr[j - 1];
+      arr[j] = arr[j] % (1E9 + 7);
     }
   }
   
-  return ans[n - 1];
+   return arr.reduce((a, b) => a + b, 0) % (1E9 + 7);
 };

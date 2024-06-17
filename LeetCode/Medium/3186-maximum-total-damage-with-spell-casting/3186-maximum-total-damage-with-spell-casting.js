fb2400d7381fc3884d96e82240c4abc592b89275
Sujin Lee <91821887+zzinLee@users.noreply.github.com>
2024-06-17 17:28:10 +0900

    Time: 438 ms (100.00%), Space: 84.7 MB (100.00%) - LeetHub

---
+++ LeetCode/Medium/3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[]} power
+ * @return {number}
+ */
+var maximumTotalDamage = function(power) {
+  const powerMap = new Map();
+  
+  for (const strength of power) {
+    if (powerMap.has(strength)) {
+      powerMap.set(strength, powerMap.get(strength) + 1);
+    } else {
+      powerMap.set(strength, 1);
+    }
+  }
+  
+  const powers = Array.from(powerMap.keys()).sort((a, b) => a - b);
+  const len = powers.length;
+  const DP = new Array(len).fill(0);
+  
+  DP[0] = powers[0] * powerMap.get(powers[0]);
+  
+  for (let i = 1; i < len; i++) {
+    const currentPower = powers[i];
+    const currentTotal = currentPower * powerMap.get(currentPower);
+    
+    DP[i] = DP[i - 1];
+    
+    let prevIndex = i - 1;
+    
+    while (prevIndex >= 0 && 
+           powers[prevIndex] === currentPower - 1 ||
+           powers[prevIndex] === currentPower - 2 ||
+           powers[prevIndex] === currentPower + 1 ||
+           powers[prevIndex] === currentPower + 2
+          ) { prevIndex--; }
+    
+    if (prevIndex >= 0) {
+      DP[i] = Math.max(DP[i], DP[prevIndex] + currentTotal);
+    } else {
+      DP[i] = Math.max(DP[i], currentTotal);
+    }
+  }
+  
+  return DP[len - 1];
+};
